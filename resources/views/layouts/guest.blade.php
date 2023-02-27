6d69ebe59c94da3f0fb6e04fcca48b7df56aee3a
NolanEvard <Nolan.EVARD@cpnv.ch>
2023-02-27 12:16:32 +0100

    WIP + Deleted useless tests

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,6 +21,9 @@
                     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
                 </a>
             </div>
+            <div class="sm:justify-center pt-6 sm:pt-0 bg-gray-100 text-xl">
+                WeekmealsPlanner
+            </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
