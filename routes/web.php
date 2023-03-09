5bf8a5cc71f087bb2024da7388651886af158f8a
NolanEvard <Nolan.EVARD@cpnv.ch>
2023-03-09 15:22:45 +0100

    Code has been cleaned up

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
-
 Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
