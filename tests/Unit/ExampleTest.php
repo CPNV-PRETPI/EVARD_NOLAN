58b3583bd45beace166f643d6fe5220bde96f134
NolanEvard <Nolan.EVARD@cpnv.ch>
2023-03-10 10:40:08 +0100

    Cleaned up a bit more + added documentation to version control

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example. This test is here to keep the Unit folder when using version control
+     */
+    public function test_that_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
