4022821eb3653c03894f867ccbea1b5de96fa0cb
NolanEvard <Nolan.EVARD@cpnv.ch>
2023-03-26 21:19:25 +0200

    WIP

--- resources/views/planning/display.blade.php
+++ resources/views/planning/display.blade.php
@@ -5,18 +5,26 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-4">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    @if($planning)
-                        @php
-                            echo($planning->weeknumber . '</br>');
-                            echo($planning->year . '</br>');
-                        @endphp
-                    @else
-                        Empty planning
-                    @endif
+            <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
+                {{--        Selected week information + week switch buttons        --}}
+                <div class="py-2 flex flew-row justify-center">
+                    <div class="rounded-md border-2 border-gray-600">
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-gray-600">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                            </svg>
+                        </a>
+                    </div>
+                    <div class=" mx-6 text-center text-gray-900">Semaine du 20.03.2023 au 26.03.2023</div>
+                    <div class="rounded-md border-2 border-gray-600">
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-gray-600">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                            </svg>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
